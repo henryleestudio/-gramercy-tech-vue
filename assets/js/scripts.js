beffc33760c6d0b3d52fab8c14ef76008c50bc31
Henry Lee - Dev <henrylee.contact@gmail.com>
2023-11-02 03:22:18 -0400

    style directory links

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -11,23 +11,12 @@
 // Write your JS below
 
 $(function () {
-  // $('input[type="submit"]').on('submit', function(){
-
-  // })
-
   $(document).on('submit', 'form', function () {
     $('button').css({
       display: 'none'
     });
     $('h1').hide();
   });
-
-  // $("#id_form").on("submit", function(){
-  //   $('#ele_id').show();
-  //   $('#ele_id').hide();
-
-  //   return false;
-  // })
 });
 
 /***/ }),
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,25 +1,15 @@
 // Write your JS below
 
 $(function() {
-  
-// $('input[type="submit"]').on('submit', function(){
-
-// })
 
 $(document).on('submit','form',function(){
   
   $('button').css({
     display: 'none',
-});
+    });
 
   $('h1').hide();
 
 });
 
-// $("#id_form").on("submit", function(){
-//   $('#ele_id').show();
-//   $('#ele_id').hide();
-
-//   return false;
-// })
 });
